9376e8594342a75c81a6afb30ba15e1952d2a3e2
freegate78 <freegate78@gmail.com>
2020-06-03 22:58:46 +0300

    сделал метод     private static List<Class> getLoggedMethods(Method[] methods, Class classOfAnnotation) вызываю его в createmyclass он заполняет аррайлист который уже дергается при инвоке

--- HW09-ByteCodes/src/main/java/com/akproductions/proxycalldemo/Ioc.java
+++ HW09-ByteCodes/src/main/java/com/akproductions/proxycalldemo/Ioc.java
@@ -7,12 +7,27 @@ import com.akproductions.testlogging.annotations.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ioc {
-    static final boolean EnableLogging = com.akproductions.testlogging.TestLoggingInterface.class.getDeclaredMethods()[0].isAnnotationPresent(Log.class);
+    static ArrayList<Class> LoggedMethods;
     private Ioc() {
     }
+
+    private static List<Class> getLoggedMethods(Method[] methods, Class classOfAnnotation){
+        ArrayList<Class> varLoggedMethods = new ArrayList(10);
+        varLoggedMethods.add(classOfAnnotation);
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(classOfAnnotation)){
+                varLoggedMethods.add(method.getClass());
+            }
+        }
+        return varLoggedMethods;
+    }
+
     public static TestLoggingInterface createMyClass() {
+        LoggedMethods= (ArrayList<Class>) getLoggedMethods(TestLoggingInterface.class.getDeclaredMethods(),Log.class);
         return (TestLoggingInterface) Proxy.newProxyInstance(com.akproductions.testlogging.TestLogging.class.getClassLoader(),
                 com.akproductions.testlogging.TestLogging.class.getInterfaces(),
                 new DemoInvocationHandler(new TestLogging()));
@@ -27,7 +42,7 @@ public class Ioc {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if(EnableLogging){System.out.println("[LOG] invoking method:" + method + " with args: "+ args[0]);}
+            if(LoggedMethods.contains(method.getClass())) {System.out.println("[LOG] invoking method:" + method + " with args: "+ args[0]);}
             return method.invoke(myClass, args);
         }
 
